d740624fadaa1ba3990d2a04b3206c40d817bfec
Leonardo Martin <leonardo.martin.95@hotmail.com>
2023-12-20 21:25:34 -0300

    icones

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 var numeroCliente = 0;
 var nomeCliente = "";
 var enderecoCliente = "";
+
+$('.card').click(ClickCardProduto());
+$('.icone-fechar').click(ClickFecharCard());
+
+function ClickCardProduto(){
+    console.log('abre');
+}
+
+function ClickFecharCard(){
+    console.log('fecha');
+}
